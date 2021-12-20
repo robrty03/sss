2ee8808641c9741ca64ed4e9f6c612191753c560
robrty03 <96355301+robrty03@users.noreply.github.com>
2021-12-21 00:58:33 +0700

    Update dgb.sh

--- dgb.sh
+++ dgb.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-sudo apt update
-sudo apt install screen -y
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 ./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DL4UDGn1BJ1KhnWaSZUgZMji5GE1ZcqpCp -p c=DGB -t 4
